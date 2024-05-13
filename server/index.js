326f080fc5cb9d7a4a74eb55f34c2920d2a76320
tRelax <tinmil7@gmail.com>
2024-05-13 23:02:12 +0200

    added backend for chat and messages

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoutes");
+const chatRoute = require("./routes/chatRoute");
+const messageRoute = require("./routes/messageRoute");
 
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api/users", userRoute)
+app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 //CRUD
 //app.post || app.get || app.put || app.delete
