0b7c4776bee2f3efbf6c516f7f9c954c1c11ccbb
tRelax <tinmil7@gmail.com>
2024-05-30 01:14:34 +0200

    added auth on backend

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authToken = require("./middleware/auth");
 const userRoute = require("./routes/userRoutes");
 const chatRoute = require("./routes/chatRoute");
 const messageRoute = require("./routes/messageRoute");
+const imagesRoute = require("./routes/imageRoute");
 
 const app = express();
 require("dotenv").config();
 
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 app.use(cors());
 app.use("/api/users", userRoute);
+app.use("/api/images", imagesRoute);
+app.use(authToken);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
@@ -24,9 +28,9 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
-app.listen(port, (req, res) =>{
+app.listen(port, (req, res) => {
     console.log(`Server running on port: ${port}`)
 })
 
-mongoose.connect(uri, {}).then(()=> console.log("MongoDB connection established"))
+mongoose.connect(uri, {}).then(() => console.log("MongoDB connection established"))
     .catch((err) => console.error("MongoDB connection failed: ", err.message));
---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+const userModel = require("../models/userModel");
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const header = req.header('Authorization');
+    const token = header.split(" ")[1];
+
+    if (!token) return res.status(401).json({error: 'Access denied'});
+
+    try {
+        const jwtKey = process.env.JWT_SECRET_KEY;
+        const decoded = jwt.verify(token, jwtKey);
+        req.userId = decoded.id;
+        next();
+    } catch (e) {
+        res.status(401).json({error: 'Invalid token'});
+    }
+}
+
+module.exports = verifyToken;
